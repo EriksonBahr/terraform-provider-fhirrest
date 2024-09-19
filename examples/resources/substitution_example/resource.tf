028dbd3715ff2ded33e158b47b1e5020e4ab98d3
Reinoldo Krause Junior <reinoldo.krause@philips.com>
2024-09-19 12:34:20 -0300

    feat: Adding substitions input to replace values on files

---
+++ examples/resources/substitution_example/resource.tf
@@ -0,0 +1,8 @@
+resource "fhirrest_fhir_resource" "patient" {
+  file_path   = "${path.cwd}/patient.json"
+  file_sha256 = sha256(file("${path.cwd}/patient.json"))
+  substitutions = {
+    "name" = "John Doe",
+    "TENANT_PLACEHOLDER" = "my-tenant",
+  }
+}
